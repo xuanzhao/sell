e90ab75357ec27cc5d998287af0974a19775bf45
xuanzhao <xuanzhao89@gmail.com>
2022-11-16 14:45:19 +0800

    Spring Boot企业微信点餐系统 后端代码

---
+++ src/test/java/com/imooc/LoggerTest.java
@@ -0,0 +1,34 @@
+package com.imooc;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class LoggerTest {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Test
+    public void test1() {
+        logger.debug("debug...");
+        logger.info("info...");
+        logger.error("error...");
+    }
+
+    @Test
+    public void test2() {
+        String name = "imooc";
+        String password = "123456";
+        log.debug("debug...");
+        log.info("info...");
+        log.error("error...");
+        log.info("name: {}, password: {}", name, password);
+    }
+}
